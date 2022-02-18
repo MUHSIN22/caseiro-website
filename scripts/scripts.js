ace871e0e86876e7f1ec5e8aad0c4800ecf8b3e1
Muhsin Neyyathur <muhsinny333@gmail.com>
2022-02-18 14:43:48 +0530

    Landing page almost finished need some animations

---
+++ scripts/scripts.js
@@ -0,0 +1,126 @@
+let bannerSlideIndex = 0,
+    homeProductSlideActive = 0;
+// For product mobile slide
+let posX1,posX2;
+let slideInterval;
+
+$(document).ready(() => {
+    slideInterval = setInterval(autoSlider,5000);
+    bannerDotClickSlider();
+    productDotClickSlider();
+    if($(window).innerWidth() <= 768){
+        productTouchSlider()
+    }
+})
+
+// Banner slide functions
+const autoSlider = () => {
+    let direction = 'forward';
+    if( bannerSlideIndex === 2){
+        direction = 'backward'
+    }else if( bannerSlideIndex === 0){
+        direction = 'forward'
+    }
+    
+    direction === "forward" ? bannerSlideIndex++: bannerSlideIndex--;
+    
+    $(".carousel").css("margin-left",`-${ bannerSlideIndex*100}%`)
+    $(".dot__active").removeClass("dot__active")
+    $(".dot").eq(bannerSlideIndex).addClass("dot__active");
+}
+
+const bannerDotClickSlider = () => {
+    $(".dot").click( (event) => { 
+        event.preventDefault();
+        var index = $(".dot").index(event.target)
+        bannerSlideIndex = index;
+        
+        clearInterval(slideInterval);
+        
+        $(".carousel").css("margin-left",`-${ bannerSlideIndex*100}%`)
+        $(".dot__active").removeClass("dot__active")
+        $(".dot").eq(bannerSlideIndex).addClass("dot__active");
+        
+        slideInterval = setInterval(autoSlider,5000);
+    });
+}
+
+
+// home product slide
+const slideHomeProducts = (direction) =>{
+    if(direction === 'forward' && homeProductSlideActive<2){
+        homeProductSlideActive++; 
+    }else if(direction === 'back' && homeProductSlideActive > 0){
+        homeProductSlideActive--;
+    }
+
+    setNavigationButton();
+
+    $('.products--slider').css("margin-left",`-${homeProductSlideActive*100}%`)
+    setTimeout(() => {
+        $(".product--dot__active").removeClass("product--dot__active")
+        $(".product--dot").eq(homeProductSlideActive).addClass("product--dot__active");
+    },1000)
+}
+
+const productDotClickSlider = () => {
+    $(".product--dot").click( (event) => { 
+        event.preventDefault();
+        var index = $(".product--dot").index(event.target)
+        homeProductSlideActive = index;
+        
+        setNavigationButton();
+
+        $(".products--slider").css("margin-left",`-${ homeProductSlideActive*100}%`)
+        setTimeout(() => {
+            $(".product--dot__active").removeClass("product--dot__active")
+            $(".product--dot").eq(homeProductSlideActive).addClass("product--dot__active");
+        },1000)
+    });
+}
+
+const setNavigationButton = () => {
+    if(homeProductSlideActive === 2){
+        $('.forward-navigation').css("opacity",0)
+    }else{
+        $('.forward-navigation').css("opacity",1)
+    }
+
+    if(homeProductSlideActive === 0){
+        $('.backward-navigation').css("opacity",0)
+    }else{
+        $('.backward-navigation').css("opacity",1)
+    }
+}
+
+const productTouchSlider = () => {
+    let flag = 0
+    $(".products--slider").on('touchstart',(event) => {
+        event = event || window.event;
+        if(event.type === 'touchstart'){
+            posX1 = event.touches[0].clientX
+        }else{
+            posX1 = event.clientX;
+        }
+        flag = 1
+    })
+    $(".products--slider").on('touchend',(event) => {
+        event = event || window.event;
+        let touch = event.touches[0] || event.changedTouches[0]
+        posX2 = touch.pageX 
+        if(flag === 1){
+            console.log("End");
+            productSlide();
+        }
+    })
+    
+}
+
+const productSlide = () => {
+    if(posX1 > posX2 && posX1 - posX2 > 60) {
+        slideHomeProducts("forward");
+    }else if(posX1 < posX2 && posX2 - posX1 > 60){
+        slideHomeProducts("back")
+    }
+}
+
